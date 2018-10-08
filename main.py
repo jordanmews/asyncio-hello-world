013488d8ab6c395675e89c09e82789dce13025d9
Jordan Mews <jordanmews@users.noreply.github.com>
2018-10-07 23:44:51 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+import asyncio
+
+
+async def hello(name, sleepfor):
+    print(f'hi {name} I"m giving you {sleepfor} seconds to answer me.')
+    await asyncio.sleep(sleepfor)
+    print(f'hello world, {name} here, I took all the time you gave me.. so, what do you want?')
+
+
+loop = asyncio.get_event_loop()
+loop.create_task(hello("Billy Bob", 3))
+loop.create_task(hello("Billy Alice", 1))
+
+loop.run_forever()
+loop.close()
